929d3228debb93068faa30e0ac1cff813e92935a
BrassMonkey717 <leifkormaklarson@gmail.com>
2025-12-31 21:48:38 -0600

    uncaused a new error uh

--- crit_script_test_library.py
+++ crit_script_test_library.py
@@ -1,10 +1,10 @@
 from crit_script import *
 
-@crit_script
+# @crit_script
 def get_test_values():
     return "One", "Two", "Three"
 
-@crit_script
+# @crit_script
 def debug_print(inputs:tuple[str]):
     print(inputs)
 
